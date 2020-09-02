44b8ec4e34f9b49e9ef8764ee4888fe2c264bb8f
unknown <PVinagradau@gomel.iba.by>
2020-09-02 10:51:00 +0300

    Task1

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import Header from './components/Header';
+import Caption from './components/Caption';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Caption />
+      </div>
+    );
+  }
+    
 }
 
 export default App;
---
+++ react-tutorial/src/components/Caption.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Caption.css';
+
+const caption = () => {
+    return (
+        <div className="Caption">
+            <h3>Caption</h3>
+            <hr/>
+            <p>Some text here...</p>
+        </div>
+    );
+}
+
+export default caption;
