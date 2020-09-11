b2194ad58cfb5bc40d9c061e6fe5e1a9cbdff8e0
unknown <PVinagradau@gomel.iba.by>
2020-09-11 16:27:31 +0300

    ReactJS - Task3

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './index.css';
 
+import {BsPencil, BsX, BsCheck} from 'react-icons/bs';
+
 class Card extends Component {
+
     state = {
-        isChecked: false
+        isChecked: false,
+        isEdit: false,
+        cardData : {
+            cardLogo: 'Caption',
+            text: 'Some text here...',
+        },
+        tempCardData: {
+            tempLogo: 'Caption',
+            tempText: 'Some text here...'
+        }
     }
+    
 
     changeStyles = () => {
         this.setState({isChecked : !this.state.isChecked});
+    }
+    changeEditStatus = () => {
+        this.setState({isEdit : !this.state.isEdit, isChecked: false});
+    }
+    handleChangeLogo = (event) => {
+        console.log('piu');
+        this.setState({tempCardData: {tempLogo: event.target.value, tempText: this.state.tempCardData.tempText}});
+    }
+    handleChangeText = (event) => {
+        console.log('piu-piu');
+        this.setState({tempCardData: {tempText: event.target.value, tempLogo: this.state.tempCardData.tempLogo}});
+    }
+    changeCardInfo = () => {
+        console.log('piu-piu-piu');
+        this.setState({cardData: {cardLogo: this.state.tempCardData.tempLogo, text: this.state.tempCardData.tempText}, isChecked: false});
+        this.changeEditStatus();
     }
     
     render() {
@@ -15,13 +44,45 @@ class Card extends Component {
             <div className="Card">
                 <div className="textMarg">
                     <div className={this.state.isChecked ? "green" : null}>
-                        <label className="bold">Caption</label>
-                        <input type="checkbox" className="topRight" onClick={this.changeStyles} />
+                        <div className="headCard">
+                            {this.state.isEdit ? 
+                                (<input 
+                                    type="text" 
+                                    placeholder={this.state.cardData.cardLogo}
+                                    value={this.state.tempCardData.tempLogo || ''}
+                                    onChange={this.handleChangeLogo}
+                                    className="block"
+                                />) : 
+                                (<p className="bold block">{this.state.cardData.cardLogo}</p>) 
+                            }
+                            <div className="topRight">
+                                {this.state.isEdit ? 
+                                    (<div className="block">
+                                        <BsCheck className="blue" onClick={this.changeCardInfo} />
+                                        <BsX className="red" onClick={this.changeEditStatus} />
+                                    </div>) : 
+                                    (<div className="block">
+                                        <BsPencil className="blue" onClick={this.changeEditStatus} />
+                                        <input type="checkbox" checked={this.state.isChecked} onChange={this.changeStyles} />  
+                                    </div>)
+                                }
+                            </div>
+                        </div>
                         <hr />
-                        <p>Some text here...</p>
+                        {this.state.isEdit ? 
+                            (<input 
+                                type="text" 
+                                placeholder={this.state.cardData.text}
+                                value={this.state.tempCardData.tempText || ''} 
+                                onChange={this.handleChangeText} 
+                                className="block"
+                            />) :
+                            (<p className="block">{this.state.cardData.text}</p>)
+                        }
+                        
                     </div>
                 </div>
-            </div>
+            </div>            
         );
     }
     
